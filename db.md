625f5ab6d09c597fda6eb8ddf9f8f071c36638ca
ndhuy <dhuy221001@gmail.com>
2023-04-21 16:17:46 +0700

    update UI

---
+++ db.md
@@ -0,0 +1,29 @@
+Collections: post
+  - ID
+  - title
+  - slug
+  - mage
+  - createAt
+  - status: 1(approved) 2(pending) 3(reject)
+  - content
+  - userId
+  - categoryId
+
+Collections : Category
+  - id
+  - title
+  - slug
+  - status: 1(approved) 2(pending)
+  - createdAt
+
+
+Collections : User
+  - id
+  - displayName
+  - email
+  - avatar
+  - password
+  - status: 1(active) 2(pending) 3(ban)
+  - role: 1(admin) 2(Mod) 3(User)
+  - permissions (optional)
+  - createdAt
--- src/module/post/PostAddNew.js
+++ src/module/post/PostAddNew.js
@@ -1,76 +1,135 @@
-import { Button } from "components/button";
-import { Checkbox, Radio } from "components/checkbox";
-import { Dropdown } from "components/dropdown";
-import { Field } from "components/field";
-import { Input } from "components/input";
-import { Label } from "components/label";
-import React from "react";
-import { useForm } from "react-hook-form";
-import styled from "styled-components";
+import { Button } from 'components/button';
+import { Checkbox, Radio } from 'components/checkbox';
+import { Dropdown } from 'components/dropdown';
+import { Field } from 'components/field';
+import { Input } from 'components/input';
+import { Label } from 'components/label';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import slugify from 'slugify';
+import styled from 'styled-components';
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
+import { postStatus } from 'utils/constants';
 const PostAddNewStyles = styled.div``;
 
 const PostAddNew = () => {
-  const { control, watch, setValue } = useForm({
-    mode: "onChange",
+  //Lấy storage
+  const { control, watch, setValue, handleSubmit } = useForm({
+    mode: 'onChange',
     defaultValues: {
-      status: "",
-      category: "",
+      title: "",
+      slug: "",
+      status: 2,
+      image: "",
+      category: {},
+      user: {},
     },
   });
-  const watchStatus = watch("status");
-  const watchCategory = watch("category");
-  console.log("PostAddNew ~ watchCategory", watchCategory);
+  const watchStatus = watch('status');
+  const watchCategory = watch('category');
+  console.log('PostAddNew ~ watchStatus', watchStatus);
+
+  //Handle add new posts
+  const addPostHandler = values => {
+    const cloneValues = {...values}
+    cloneValues.slug = slugify(values.slug || values.title);
+    cloneValues.status = Number(values.status)
+    console.log('values:', cloneValues);
+  };
+
+  //Handle Upload File Image
+  const handleUploadImage = e => {
+    const storage = getStorage();
+    const file = e.target.files[0];
+    console.log('file:', file);
+    if (!file) return;
+    // Upload file to the object 'images/'
+    const storageRef = ref(storage, 'images/' + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(
+      'state_changed',
+      snapshot => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+          default:
+            console.log('Nothing at all');
+        }
+      },
+      error => {
+        console.log('Upload error: ' + error);
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
+          console.log('File available at', downloadURL);
+        });
+      }
+    );
+  };
+
   return (
     <PostAddNewStyles>
       <h1 className="dashboard-heading">Add new post</h1>
-      <form>
+      <form onSubmit={handleSubmit(addPostHandler)}>
         <div className="grid grid-cols-2 gap-x-10 mb-10">
           <Field>
             <Label>Title</Label>
             <Input
               control={control}
               placeholder="Enter your title"
-              name="title"
-            ></Input>
+              name="title"></Input>
           </Field>
           <Field>
             <Label>Slug</Label>
             <Input
               control={control}
               placeholder="Enter your slug"
-              name="slug"
-            ></Input>
+              name="slug"></Input>
           </Field>
         </div>
         <div className="grid grid-cols-2 gap-x-10 mb-10">
+          <Field>
+            <Label>Image</Label>
+            <input type="file" name="image " onChange={handleUploadImage} />
+          </Field>
           <Field>
             <Label>Status</Label>
             <div className="flex items-center gap-x-5">
               <Radio
                 name="status"
                 control={control}
-                checked={watchStatus === "approved"}
-                onClick={() => setValue("status", "approved")}
-                value="approved"
-              >
+                checked={Number(watchStatus) === postStatus.APPROVER}
+                value={postStatus.APPROVER}>
                 Approved
               </Radio>
               <Radio
                 name="status"
                 control={control}
-                checked={watchStatus === "pending"}
-                onClick={() => setValue("status", "pending")}
-                value="pending"
-              >
+                checked={Number(watchStatus) === postStatus.PENDING}
+                value={postStatus.PENDING}>
                 Pending
               </Radio>
               <Radio
                 name="status"
                 control={control}
-                checked={watchStatus === "reject"}
-                onClick={() => setValue("status", "reject")}
-                value="reject"
-              >
+                checked={Number(watchStatus) === postStatus.REJECTED}
+                value={postStatus.REJECTED}>
                 Reject
               </Radio>
             </div>
